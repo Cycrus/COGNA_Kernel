4215d1a1ad2b40484032013f0aa2902e5c98e7b5
Cycrus <marxcyril@gmail.com>
2021-02-16 19:23:04 +0100

    Started Doxygen style in Connection class

--- inc/Connection.hpp
+++ inc/Connection.hpp
@@ -1,3 +1,23 @@
+/**
+ * @file Connection.hpp
+ * @author Cyril Marx (https://github.com/cycrus)
+ *
+ * @brief A class connecting the different neurons in the network. It also is responsible for learning behavior.
+ *
+ * **Note:**
+ * The connection type is always nested inside a neuron and connects this neuron to
+ * another neuron or another connection. When connected to a neuron, it sends
+ * an impulse to activate the following neuron. When connected to another connection,
+ * it sends an impulse that the following connection should get weaker or stronger.
+ *
+ * It can be configured to learn each time when sending an impulse. If it can
+ * sensitize, it gets stronger each time a strong impulse is sent, if it can
+ * habituate, it gets weaker each time a weak impulse is sent.
+ *
+ * @date 2021-02-16
+ *
+ */
+
 #ifndef INCLUDE_CONNECTION_HPP
 #define INCLUDE_CONNECTION_HPP
 
