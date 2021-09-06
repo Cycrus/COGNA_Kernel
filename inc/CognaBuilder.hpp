9d2fec353343d25b4a73369d2f7f16e679b2c381
Cycrus <marxcyril@gmail.com>
2021-09-06 23:52:33 +0200

    Partially implemented search algorithm for starting nodes of subnet connections.

--- inc/CognaBuilder.hpp
+++ inc/CognaBuilder.hpp
@@ -65,6 +65,7 @@ private:
     int load_connections(NeuralNetwork *nn, nlohmann::json network_json);
     int load_network_parameter(NeuralNetwork *nn, nlohmann::json network_json);
     int load_network(std::string network_name);
+    std::vector<nlohmann::json> find_starting_points(int source_network_id, nlohmann::json source_connection);
     std::vector<nlohmann::json> find_end_points(int source_network_id, int curr_network_id, std::vector<nlohmann::json> source_connections);
     void connect_subnet_endpoints(unsigned int curr_network_id);
     int connect_subnet_outputs(unsigned int curr_network_id, NeuralNetwork *source_network);
