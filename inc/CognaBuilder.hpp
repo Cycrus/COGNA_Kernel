72dd602476faeb984844c9d681c64e5f39c8206f
Cycrus <marxcyril@gmail.com>
2021-09-07 23:17:17 +0200

    Finished first implementation of looking for subnet connection starting and end points.

--- inc/CognaBuilder.hpp
+++ inc/CognaBuilder.hpp
@@ -66,10 +66,8 @@ private:
     int load_network_parameter(NeuralNetwork *nn, nlohmann::json network_json);
     int load_network(std::string network_name);
     std::vector<nlohmann::json> find_starting_points(int source_network_id, nlohmann::json source_connection);
-    std::vector<nlohmann::json> find_end_points(int source_network_id, int curr_network_id, std::vector<nlohmann::json> source_connections);
+    std::vector<nlohmann::json> find_end_points(int source_network_id, nlohmann::json source_connection);
     void connect_subnet_endpoints(unsigned int curr_network_id);
-    int connect_subnet_outputs(unsigned int curr_network_id, NeuralNetwork *source_network);
-    int connect_subnet_inputs(unsigned int curr_network_id, NeuralNetwork *source_network);
     int connect_subnetworks();
 
     float load_neuron_parameter(nlohmann::json neuron, std::string parameter, std::string neuron_type);
